b87a50b1a574f795ad2fd9809fff9c8bc2ad3e72
GhadeerHamed <eng.gh.a.h93@gmail.com>
2025-11-05 03:24:29 +0300

    Implement meal management with data models and UI components

---
+++ lib/screens/meals.dart
@@ -0,0 +1,59 @@
+import 'package:flutter/material.dart';
+import 'package:meals_app/models/meal.dart';
+
+class MealsScreen extends StatelessWidget {
+  const MealsScreen({super.key, required this.title, required this.meals});
+
+  final String title;
+  final List<Meal> meals;
+
+  @override
+  Widget build(BuildContext context) {
+    Widget content = ListView.builder(
+      itemCount: meals.length,
+      itemBuilder: (ctx, index) {
+        final meal = meals[index];
+        return ListTile(
+          title: Text(meal.title),
+          leading: Image.network(meal.imageUrl),
+          subtitle: Text(
+            '${meal.duration} min | ${meal.complexity.name} | ${meal.affordability.name}',
+          ),
+        );
+      },
+    );
+    if (meals.isEmpty) {
+      content = Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Text(
+              'Uh oh ... nothing here!',
+              style: Theme.of(context).textTheme.headlineLarge!.copyWith(
+                color: Theme.of(context).colorScheme.onSurface,
+              ),
+            ),
+            const SizedBox(height: 16),
+            Text(
+              'Try selecting a different category!',
+              style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                color: Theme.of(
+                  context,
+                ).colorScheme.onSurface.withValues(alpha: 0.7),
+              ),
+            ),
+          ],
+        ),
+      );
+    }
+
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(title),
+      ),
+      body: Center(
+        child: content,
+      ),
+    );
+  }
+}
