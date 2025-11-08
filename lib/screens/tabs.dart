921102231d1ddbfd637d708004ef4c84ca5df632
GhadeerHamed <eng.gh.a.h93@gmail.com>
2025-11-08 05:18:14 +0300

    Refactor TabsScreen to improve favorite meal handling and display

--- lib/screens/tabs.dart
+++ lib/screens/tabs.dart
@@ -14,15 +14,29 @@ class _TabsScreenState extends State<TabsScreen> {
   final List<Meal> _favoriteMeals = [];
   int _selectedPageIndex = 0;
 
+  void showInfoDialog(String message) {
+    ScaffoldMessenger.of(context).clearSnackBars();
+    ScaffoldMessenger.of(context).showSnackBar(
+      SnackBar(
+        content: Text(message),
+      ),
+    );
+  }
+
   void _toggleMealFavoriteStatus(Meal meal) {
     final isExisting = _favoriteMeals.contains(meal);
-    setState(() {
-      if (isExisting) {
+
+    if (isExisting) {
+      setState(() {
         _favoriteMeals.remove(meal);
-      } else {
+      });
+      showInfoDialog('Meal removed from favorites.');
+    } else {
+      setState(() {
         _favoriteMeals.add(meal);
-      }
-    });
+      });
+      showInfoDialog('Meal added to favorites.');
+    }
   }
 
   void _selectPage(int index) {
@@ -40,7 +54,7 @@ class _TabsScreenState extends State<TabsScreen> {
 
     if (_selectedPageIndex == 1) {
       activePage = MealsScreen(
-        meals: [],
+        meals: _favoriteMeals,
         onToggleFavorite: _toggleMealFavoriteStatus,
       );
       activePageTitle = "Your Favorites";
